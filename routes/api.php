4b25674303357679e695cf15afb1e4d2e2b79b5e
Petar Mihaylovski <459725@Student.saxion.nl>
2021-10-12 15:05:40 +0200

    cors added to the api

--- routes/api.php
+++ routes/api.php
@@ -28,9 +28,12 @@ Route::delete('/rooms/{room}', [RoomController::class, 'destroy']);
 Route::get('/rooms/search/{date}', [RoomController::class, 'search']);*/
 
 //shop routes
+Route::middleware(['cors'])->group(function () {
+    Route::get('/shop', [ShopController::class, 'index']);
+});
+
 Route::post('/shop', [ShopController::class, 'store']);
 Route::get('/shop/{id}', [ShopController::class, 'show']);
-Route::get('/shop', [ShopController::class, 'index']);
 Route::get('/shop/{id}/product', [ShopController::class, 'getAllProdutsFromShop']);
 
 //user routes
