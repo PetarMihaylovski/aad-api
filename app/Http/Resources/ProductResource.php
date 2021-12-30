25b424d798902adb9db971874d5a4c3dde9e918a
petar <459725@student.saxion.nl>
2021-12-31 00:55:31 +0100

    image and product resource and collection modeled

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'shopId' => $this->shop_id,
+            'name' => $this->name,
+            'price' => $this->price,
+            'inStock' => $this->stock > 0,
+            'stock' => $this->stock,
+            'updated'=>$this->updated_at,
+            'images' => ImageResource::collection($this->whenLoaded('images'))
+        ];
+    }
+}
