ef533a71f0a42ee55b06d1ff9a1c10c492785300
petar <459725@student.saxion.nl>
2022-01-03 17:40:56 +0100

    callback before product gets deleted to delete its images created

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,14 +14,34 @@ class Product extends Model
             'name',
             'price',
             'stock',
-            'category'
+            'category',
+            'shop_id'
         ];
 
-    public function shop(){
+    public function shop()
+    {
         return $this->belongsTo(Shop::class, "shop_id");
     }
 
-    public function images(){
+    public function images()
+    {
         return $this->hasMany(Image::class, "product_id");
+    }
+
+
+    /**
+     * called before the instance gets deleted.
+     *
+     * deletes all the images for a given product, before the product itself gets deleted
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($product) {
+            $product->images()->delete();
+        });
     }
 }
