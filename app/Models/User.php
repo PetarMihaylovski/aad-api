3b4a1b117d3bef7fd1bb39e6b9333aae148b540b
petar <459725@student.saxion.nl>
2022-01-04 17:30:34 +0100

    relationship remodeled, user resource created

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -44,7 +43,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function getShopRelation(){
-        return $this->hasOne('App\Models\Shop', 'user-id', 'id');
+    public function shop(){
+        return $this->hasOne(Shop::class, 'user_id');
     }
 }
