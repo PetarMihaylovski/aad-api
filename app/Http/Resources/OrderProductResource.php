b5fa76c6ffb44d82855585475ca64b3c8aaa18b0
petar <459725@student.saxion.nl>
2022-01-05 14:18:05 +0100

    resources and collections for order and orderproduct created

---
+++ app/Http/Resources/OrderProductResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'orderId' => $this->order_id,
+            'productId'=>$this->product_id,
+            'quantity' => $this->quantity,
+            'total' => $this->total
+        ];
+    }
+}
