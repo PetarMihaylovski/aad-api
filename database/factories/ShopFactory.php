f886d35a4e6c3b98cf99adf308535c77413e33ef
petar <459725@student.saxion.nl>
2022-01-08 19:02:49 +0100

    user and shop factories created

---
+++ database/factories/ShopFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Shop;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
+
+class ShopFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Shop::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $userIds = DB::table('uses')->pluck('id')->all();
+        return [
+            'user_id' => $this->faker->randomElement($userIds),
+            'name' => $this->faker->name(),
+            'description' => $this->faker->sentence()
+        ];
+    }
+}
